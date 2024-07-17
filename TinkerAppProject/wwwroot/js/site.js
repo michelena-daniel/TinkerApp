66e1b2f4eed6d08c9cd2be07e16940977b1ee616
Daniel Romero <danielromero.sem@gmail.com>
2024-07-17 13:51:51 +0200

    Refactor JS to reuse code

--- TinkerAppProject/wwwroot/js/site.js
+++ TinkerAppProject/wwwroot/js/site.js
@@ -2,3 +2,28 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+
+function showPassword(passtype) {
+    var input = document.getElementById("password-input");
+    if (passtype == "confirm") {
+        input = document.getElementById("confirm-input");
+        var eyeIconConfirm = document.getElementById("eye-icon-confirm");
+        if (eyeIconConfirm.className == "bi bi-eye-slash") {
+            eyeIconConfirm.className = "bi bi-eye-fill";
+        } else {
+            eyeIconConfirm.className = "bi bi-eye-slash";
+        }
+    } else if (passtype == "pass") {
+        var eyeIconPass = document.getElementById("eye-icon-pass");
+        if (eyeIconPass.className == "bi bi-eye-slash") {
+            eyeIconPass.className = "bi bi-eye-fill";
+        } else {
+            eyeIconPass.className = "bi bi-eye-slash";
+        }
+    }
+    if (input.type === "password") {
+        input.type = "text";
+    } else {
+        input.type = "password";
+    }
+}
