6aa895076b2cc777dc7aa514a1a4505c545c15b1
Daniel Romero <45939723+michelena-daniel@users.noreply.github.com>
2024-07-21 21:39:08 +0200

    Refactor Passwords on front dashboard (#7)
    * Refactor JS to reuse code
    
    * Refactor JS, add event listener to avoid code duplication
    
    * Remove unnecessary ID's and comment
    
    * Update Login.cshtml

--- TinkerAppProject/wwwroot/js/site.js
+++ TinkerAppProject/wwwroot/js/site.js
@@ -2,3 +2,22 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+
+function showPassword(eyeIcon) {
+    var inputId = eyeIcon.getAttribute("data-target");
+    var input = document.getElementById(inputId);
+
+    if (input.type === "password") {
+        input.type = "text";
+    } else {
+        input.type = "password";
+    }
+
+    if (eyeIcon.classList.contains("bi-eye-slash")) {
+        eyeIcon.classList.remove("bi-eye-slash");
+        eyeIcon.classList.add("bi-eye-fill");
+    } else {
+        eyeIcon.classList.remove("bi-eye-fill");
+        eyeIcon.classList.add("bi-eye-slash");
+    }
+}
