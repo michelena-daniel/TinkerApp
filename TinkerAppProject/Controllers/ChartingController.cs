0e8d2fcb1aa144d11cfcc8f20b9ef245268a7675
Daniel Romero <45939723+michelena-daniel@users.noreply.github.com>
2024-07-22 18:04:25 +0200

    Add Chart.js and base view structure with placeholder charts (#9)

---
+++ TinkerAppProject/Controllers/ChartingController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace TinkerAppProject.Controllers
+{
+    [Authorize]
+    public class ChartingController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
