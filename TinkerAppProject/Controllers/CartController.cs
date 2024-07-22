936349386455aefd12d35a8f00f88ee50a963ee9
Daniel Romero <45939723+michelena-daniel@users.noreply.github.com>
2024-07-22 03:29:03 +0200

    Add logo and cart base design (#8)

---
+++ TinkerAppProject/Controllers/CartController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TinkerAppProject.Controllers
+{
+    public class CartController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
