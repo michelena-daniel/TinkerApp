f634d55795e03a256cd6e8060235aebd756f900b
Daniel Romero <danielromero.sem@gmail.com>
2024-08-26 22:30:06 +0200

    Fix dropdown to correctly show chart options

--- TinkerAppProject/Models/Charting/ChartModel.cs
+++ TinkerAppProject/Models/Charting/ChartModel.cs
@@ -1,8 +1,11 @@
-﻿namespace TinkerAppProject.Models.Charting
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TinkerAppProject.Models.Charting
 {
     public class ChartModel
     {
         public ChartTypeEnum Type { get; set; }
+        [Range(1, 100)]
         public int MonthRange { get; set; }
         public LabelTypeEnum LabelType { get; set; }
     }
