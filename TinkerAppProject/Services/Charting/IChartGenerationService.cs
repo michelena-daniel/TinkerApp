2b4dd5d246a85d864e0e43bc13e3a09e2e19c439
Daniel Romero <danielromero.sem@gmail.com>
2024-08-27 16:34:12 +0200

    Add year to differentiate expense's month, change form to action link

--- TinkerAppProject/Services/Charting/IChartGenerationService.cs
+++ TinkerAppProject/Services/Charting/IChartGenerationService.cs
@@ -27,15 +27,14 @@ namespace TinkerAppProject.Services.Charting
             {
                 var expenses = await _expenseRepository.GetAllExpensesByUser(userId);
                 var startDate = DateTime.Now.AddMonths(-model.MonthRange + 1);
-
-                response.Dataset = Enumerable.Range(0, model.MonthRange)
-                        .Select(i => new { Year = startDate.AddMonths(i) })
-                        .OrderByDescending(i => i.Year)
-                        .ThenByDescending(i => i.Year.Month)
-                        .Select(i => expenses.Where(e => e.DayPaid.Month == i.Year.Month && e.DayPaid.Year == i.Year.Year)
-                        .Select(e => e.AmountPaid)
-                        .Sum())
-                        .ToList();
+                var months = Enumerable.Range(0, model.MonthRange)
+                    .Select(i => startDate.AddMonths(i))
+                    .OrderByDescending(date => date);
+                response.Dataset = months
+                    .Select(month => expenses
+                        .Where(expense => expense.DayPaid.Month == month.Month && expense.DayPaid.Year == month.Year)
+                        .Sum(expense => expense.AmountPaid))
+                    .ToList();
             }
             return response;
         }
@@ -46,7 +45,8 @@ namespace TinkerAppProject.Services.Charting
             var date = DateTime.UtcNow;
             for (var i = 0; i < range; i++)
             {
-                result.Add(date.AddMonths(-i).ToString("MMMM"));
+                var sequencedDate = date.AddMonths(-i);
+                result.Add(sequencedDate.ToString("MMMM")+$" {sequencedDate.Year.ToString().Substring(2)}");
             }
             return result;
         }
